643ffd853decf4eac681d049ea94241f8236533b
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2023-04-24 15:38:34 -0700

    Started creating the files for the recipe blog. Am working on adding some new images. Just getting the file structure together. Database and all the routes are still todo.

---
+++ server/routes/recipeRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const recipeController = require('../controllers/recipeController');
+
+// App Routes
+
+router.get('/', recipeController.homepage);
+
+
+
+
+
+
+
+
+
+module.exports = router;
