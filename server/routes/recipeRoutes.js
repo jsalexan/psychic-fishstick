cfffa135e4e9b18cab01a6c3c387219d30b1cc83
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2023-04-25 16:01:00 -0700

    Connected the database and am now working on creating content to test it out. Added more models and started on new views. Also updated routes and controller.

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -5,6 +5,7 @@ const recipeController = require('../controllers/recipeController');
 // App Routes
 
 router.get('/', recipeController.homepage);
+router.get('/categories', recipeController.showCategories)
 
 
 
